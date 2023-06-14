e3dd2cc92b53664e22810f8012e337384ddca56d
dadyvenom <135177210+dadyvenom@users.noreply.github.com>
2023-06-14 20:56:10 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, idle
+#'‹ ٰ💸 ⇣ سورس الفراعنة ⇣ 💸 › .'#
+from pyromod import listen
+
+
+
+bot = Client(
+    "mo",
+    api_id=26022994,
+    api_hash="2e84a6b68bd6b5a79f46e8192668e0ea",
+    bot_token="6013874411:AAF95252Q7i6sqOTpDEkBKAca949CxIvIJU",#توكن المصنع
+    plugins=dict(root="MHelal")
+    )
+
+async def start_helalbot():
+    print("تم تشغيل الصانع بنجاح..💗")
+    await bot.start()
+    hh = "@R_N_B1"#يوزر المطور المصنع
+    try:
+        await bot.send_message(hh, "**تم تشغيل الصانع بنجاح ...🥀**")
+    except:
+        pass
+    await idle()
